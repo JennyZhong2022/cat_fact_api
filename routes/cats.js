0e86fe8f65fa9ceb8f7a57b090bee3c9a9105e70
Xiao Zhong <zhongxiao2017@gmail.com>
2023-10-29 00:32:01 +1100

    api fetched

---
+++ routes/cats.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+
+const url = process.env.CAT_FACT_API
+console.log('url',url);
+
+router.get('/', async (req, res) => {
+  try {
+    const userData = await fetch('https://catfact.ninja/fact')
+      .then(res => res.json());
+    res.render('cat/fact', { userData }); 
+    console.log(userData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('An error occurred while fetching data.');
+  }
+});
+
+
+module.exports = router;
